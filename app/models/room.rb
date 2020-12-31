f6ae1e00ca91c90cb19008bdd2018f9936283edf
urayuuki <76430661+urayuuki@users.noreply.github.com>
2020-12-31 18:40:04 +0900

    room_usersテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,4 @@
 class Room < ApplicationRecord
+  has_many :rooom_users
+  has_many :users, through: :rooom_users
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
   resources :users,only: [:edit, :update]
+  resources :rooms,only: [:mew, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
