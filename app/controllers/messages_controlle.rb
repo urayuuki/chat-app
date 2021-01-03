e99d15eb0032c01e170f17e94b3c2b775e5f7d97
urayuuki <76430661+urayuuki@users.noreply.github.com>
2021-01-03 12:36:33 +0900

    メッセージの表示

--- app/controllers/messages_controlle.rb
+++ app/controllers/messages_controlle.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message=Message.new
     @room= Room.find(params[:room_id])
+    @messages= @room.messages.includes(:user)
   end
     
   def create
@@ -9,7 +10,9 @@ class MessagesController < ApplicationController
     @message= @room.messages.new(message_params)
      if @message.save
           redirect_to room_messages_path(@room)
-     else render:index
+     else 
+      @messages =@room.messages.includes(:user)
+      render:index
     end
     
   end
