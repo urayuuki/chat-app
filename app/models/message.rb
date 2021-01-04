4a6a1f6c05c1c16a94e9395059080bb168c21374
urayuuki <76430661+urayuuki@users.noreply.github.com>
2021-01-04 18:59:47 +0900

    各モデルのテスト

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,14 +4,9 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validetes:content,presence: true, unless : :was_attached?
+  validates :content, presence: true, unless: :was_attached?
     def was_attached?
       self.image.attached?
       
     end
  end
-user_deta.each do |u|
-
-  puts u[:user][:profile][:name]
-  
-end
