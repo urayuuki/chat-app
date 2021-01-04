38c605f88fb95bf3e8e7f4ca0955dd38fc757d05
urayuuki <76430661+urayuuki@users.noreply.github.com>
2021-01-04 09:43:52 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,13 @@ class Message < ApplicationRecord
 
   belongs_to :room
   belongs_to :user
+  has_one_attached :image
 
   validetes:content,presence: true
+end
+
+user_deta.each do |u|
+
+  puts u[:user][:profile][:name]
+  
 end
