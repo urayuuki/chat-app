ff4ab28f33acae225c75a413cc4540dc8a9073cb
urayuuki <76430661+urayuuki@users.noreply.github.com>
2021-01-04 09:59:22 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,9 +4,12 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validetes:content,presence: true
-end
-
+  validetes:content,presence: true, unless : :was_attached?
+    def was_attached?
+      self.image.attached?
+      
+    end
+ end
 user_deta.each do |u|
 
   puts u[:user][:profile][:name]
